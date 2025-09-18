cce9c02fc3cd5923725def8967cc40f7a037ba16
Enthony <oliveiraenthony987@gmail.com>
2025-09-18 11:33:30 -0300

    Projeto Finalizado

---
+++ script.js
@@ -0,0 +1,8 @@
+const hamburgerBtn = document.querySelector('.hamburger-btn');
+const navLinks = document.querySelector('.nav-links');
+
+hamburgerBtn.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+
+    hamburgerBtn.classList.toggle('is-open');
+});
